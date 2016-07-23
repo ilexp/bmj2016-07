0b0870874d237b483f299660918b359269d6f740
j-p-e-g <j-p-e-g@users.noreply.github.com>
2016-07-23 17:59:55 +0200

    Make potion disappear properly in the pot

--- UnforeseenConsequences/Assets/Scripts/BottlePotionOnClick.cs
+++ UnforeseenConsequences/Assets/Scripts/BottlePotionOnClick.cs
@@ -3,47 +3,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnforeseenConsequences.Chemistry;
 
-public class BottlePotionOnClick : InteractiveObject
+namespace UnforeseenConsequences
 {
-	public GameObject m_potionPrefab;
-	public Transform m_potionsBar;
-
-	private List<Substance> m_potContent;
-
-	void Start()
+	public class BottlePotionOnClick : InteractiveObject
 	{
-		m_potContent = new List<Substance>();
-	}
+		public GameObject m_potionPrefab;
+		public Transform m_potionsBar;
 
-	public override void handleMouseDown()
-	{
-		// create new potion if non-empty
-		if (m_potContent.Count > 0)
+		private List<Substance> m_potContent;
+
+		void Start()
 		{
-			GameObject newPotion = Instantiate(m_potionPrefab);
-			newPotion.transform.parent = m_potionsBar;
-			newPotion.transform.position = new Vector3(0, 0, 0);
-			DraggableItem item = newPotion.GetComponent<DraggableItem>();
-			if (item)
+			m_potContent = new List<Substance>();
+		}
+
+		public override void handleMouseDown()
+		{
+			// create new potion if non-empty
+			if (m_potContent.Count > 0)
 			{
-				item.m_potionSubstance = m_potContent;
-				m_potContent.Clear();
+				GameObject newPotion = Instantiate(m_potionPrefab);
+				newPotion.transform.parent = m_potionsBar;
+				newPotion.transform.position = new Vector3(0, 0, 0);
+				DraggableItem item = newPotion.GetComponent<DraggableItem>();
+				if (item)
+				{
+					item.m_potionSubstance = m_potContent;
+					m_potContent.Clear();
+				}
+			}
+			else
+			{
+				Debug.Log("The pot is empty!");
 			}
 		}
-		else
-		{
-			Debug.Log("The pot is empty!");
-		}
-	}
 
-	public void addPotion(DraggableItem i_potion)
-	{
-		BottlePotionOnClick potScript = GetComponent<BottlePotionOnClick>();
-		if (potScript)
+		public void addPotion(DraggableItem i_potion)
 		{
-			Debug.Log("added " + i_potion.name + " to pot");
-			m_potContent.AddRange(i_potion.m_potionSubstance);
-			m_potContent = Solver.Solve(m_potContent);
+			Potion potionScript = i_potion.GetComponent<Potion>();
+			BottlePotionOnClick potScript = GetComponent<BottlePotionOnClick>();
+			if (potionScript && potScript)
+			{
+				Debug.Log("added " + i_potion.name + " to pot");
+				m_potContent.Add(potionScript.Content);
+				m_potContent = Solver.Solve(m_potContent);
+			}
 		}
 	}
 }

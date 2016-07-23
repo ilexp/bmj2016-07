4fbe8ead239a33d7b0d214be1fe0533a79253c07
j-p-e-g <j-p-e-g@users.noreply.github.com>
2016-07-23 16:41:20 +0200

    Actually add the potion to the pot (apply Solve)

--- UnforeseenConsequences/Assets/Scripts/BottlePotionOnClick.cs
+++ UnforeseenConsequences/Assets/Scripts/BottlePotionOnClick.cs
@@ -17,15 +17,33 @@ public class BottlePotionOnClick : InteractiveObject
 
 	public override void handleMouseDown()
 	{
-		GameObject newPotion = Instantiate(m_potionPrefab);
-		newPotion.transform.parent = m_potionsBar;
-		newPotion.transform.position = new Vector3(0, 0, 0);
-		DraggableItem item = newPotion.GetComponent<DraggableItem>();
-		if (item)
+		// create new potion if non-empty
+		if (m_potContent.Count > 0)
 		{
-			item.m_potionSubstance = m_potContent;
-			m_potContent.Clear();
+			GameObject newPotion = Instantiate(m_potionPrefab);
+			newPotion.transform.parent = m_potionsBar;
+			newPotion.transform.position = new Vector3(0, 0, 0);
+			DraggableItem item = newPotion.GetComponent<DraggableItem>();
+			if (item)
+			{
+				item.m_potionSubstance = m_potContent;
+				m_potContent.Clear();
+			}
+		}
+		else
+		{
+			Debug.Log("The pot is empty!");
 		}
 	}
 
+	public void addPotion(DraggableItem i_potion)
+	{
+		BottlePotionOnClick potScript = GetComponent<BottlePotionOnClick>();
+		if (potScript)
+		{
+			Debug.Log("added " + i_potion.name + " to pot");
+			m_potContent.AddRange(i_potion.m_potionSubstance);
+			m_potContent = Solver.Solve(m_potContent);
+		}
+	}
 }
--- UnforeseenConsequences/Assets/Scripts/DragTarget.cs
+++ UnforeseenConsequences/Assets/Scripts/DragTarget.cs
@@ -1,66 +1,36 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnforeseenConsequences.Chemistry;
 
 public class DragTarget : InteractiveObject
 {
-	bool m_isHovered = false;
 	public static DragTarget m_hoveredObject = null;
 
 	public void OnMouseEnter()
 	{
-		m_isHovered = true;
 		m_hoveredObject = this;
 		Debug.Log("OnMouseEnter event on " + this.gameObject.name);
 	}
 
 	public void OnMouseExit()
 	{
-		m_isHovered = false;
 		// assumes there's no overlap
 		m_hoveredObject = null;
 		Debug.Log("OnMouseExit event on " + this.gameObject.name);
 	}
-
-	void Update()
-	{
-		checkForMouseover();
-	}
-
-	protected void checkForMouseover()
-	{
-		if (m_isHovered && !EventSystem.current.IsPointerOverGameObject())
-		{
-		}
-	}
-
-	//public override void handleMouseDown()
-	//{
-	//	Debug.Log("handleMouseDOWN");
-	//}
-
-	//public override void handleMouseUp()
-	//{
-	//	Debug.Log("handleMouseUP");
-	//	if (m_draggedItem != null)
-	//	{
-	//		interactWithObject(m_draggedItem);
-	//	}
-	//}
 
 	public void interactWithObject(DraggableItem in_item)
 	{
 		if (in_item)
 		{
 			Debug.Log("attempt to interact with " + this.gameObject.name + (in_item == null ? "" : " while dragging " + in_item.name));
-			addPotion(in_item);
+			BottlePotionOnClick potScript = GetComponent<BottlePotionOnClick>();
+			if (potScript)
+			{
+				potScript.addPotion(in_item);
+			}
 			in_item.destroyObject();
 		}
-	}
-
-	void addPotion(DraggableItem i_potion)
-	{
-		// do stuff
-		Debug.Log("added " + i_potion.name + " to pot");
 	}
 }

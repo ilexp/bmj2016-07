db7c522d12138e9ef348e9eff6b1f559a0fe66cb
Adam <adam@adamslair.net>
2016-07-23 20:17:46 +0200

    Added More Effects

---
+++ UnforeseenConsequences/Assets/Scripts/Chemistry/Effects/ChangeColorEffect.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnforeseenConsequences.Chemistry
+{
+	[Serializable]
+	[CreateAssetMenu]
+	public class ChangeColorEffect : Effect
+	{
+		[SerializeField] private Color color;
+
+		public override void Trigger(Character applyTo)
+		{
+			base.Trigger(applyTo);
+			if (applyTo != null)
+			{
+				Image gfx = applyTo.GetComponent<Image>();
+				if (gfx != null)
+					gfx.color = this.color;
+			}
+		}
+	}
+}

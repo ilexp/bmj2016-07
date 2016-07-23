02017ac2487cae5b6c56d3f0b9b1869a4afcdb38
j-p-e-g <j-p-e-g@users.noreply.github.com>
2016-07-23 14:38:00 +0200

    Add basic click-handling scripts

---
+++ UnforeseenConsequences/Assets/Scripts/ClickableObject.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ClickableObject : MonoBehaviour
+{
+	bool m_isHovered = false;
+
+	public void OnMouseEnter()
+	{
+		m_isHovered = true;
+		//Debug.Log("OnMouseEnter event on " + this.gameObject.name);
+	}
+
+	public void OnMouseExit()
+	{
+		m_isHovered = false;
+		//Debug.Log("OnMouseExit event on " + this.gameObject.name);
+	}
+
+	void Update()
+	{
+		checkForMouseClick();
+	}
+
+	protected void checkForMouseClick()
+	{
+		if (m_isHovered && !EventSystem.current.IsPointerOverGameObject())
+		{
+			if (Input.GetKeyDown(KeyCode.Mouse0))
+			{
+				Debug.Log("Left Mouse click event on " + this.gameObject.name);
+				handleLeftMouseClick(Input.mousePosition);
+			}
+		}
+	}
+
+	protected void releaseDraggedItem()
+	{
+		//if (m_playerInventory)
+		//{
+		//	m_playerInventory.releaseDraggedItem();
+		//}
+	}
+
+	public virtual void handleLeftMouseClick(Vector3 in_mousePos)
+	{
+		releaseDraggedItem();
+		//if (m_player)
+		//{
+		//	m_player.moveToPosition(in_mousePos, this.gameObject);
+		//}
+	}
+}
---
+++ UnforeseenConsequences/Assets/Scripts/InteractiveObject.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractiveObject : ClickableObject
+{
+	[SerializeField]
+	private string m_objectId;
+
+	[SerializeField]
+	private string m_description;
+
+	[SerializeField]
+	Item m_usableItem;
+
+	[SerializeField]
+	GameObject m_replacementObject;
+
+	[SerializeField]
+	bool m_destroyItemAfterUse;
+
+	public override void handleLeftMouseClick(Vector3 in_mousePos)
+	{
+		//if (m_player)
+		//{
+		//	m_player.displayText(m_description);
+		//}
+	}
+
+	void interactWithObject(Item in_item)
+	{
+		Debug.Log("attempt to interact with " + this.gameObject.name + (in_item == null ? "" : " while dragging " + in_item.name));
+		if (in_item)
+		{
+			useItemOnObject(in_item);
+		}
+		else
+		{
+			simpleUseObject();
+		}
+	}
+
+	public virtual void simpleUseObject()
+	{
+	}
+
+	public virtual void useItemOnObject(Item in_item)
+	{
+		if (in_item == m_usableItem)
+		{
+			if (m_replacementObject)
+			{
+				m_replacementObject.SetActive(true);
+				this.gameObject.SetActive(false);
+
+				//if (m_destroyItemAfterUse && m_playerInventory)
+				//{
+				//	m_playerInventory.destroyDraggedItem();
+    //            }
+			}
+		}
+	}
+
+	public virtual void destroyObject()
+	{
+		this.gameObject.SetActive(false);
+	}
+}

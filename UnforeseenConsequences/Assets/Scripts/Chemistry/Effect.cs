d8ef1f25c1211407d9fad617223759eaed9d7aea
Adam <adam@adamslair.net>
2016-07-23 15:16:10 +0200

    Chemistry WiP

---
+++ UnforeseenConsequences/Assets/Scripts/Chemistry/Effect.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+
+namespace UnforeseenConsequences.Chemistry
+{
+	[Serializable]
+	public abstract class Effect : ScriptableObject
+	{
+	}
+	[Serializable]
+	[CreateAssetMenu]
+	public class CreateSubstanceEffect : Effect
+	{
+		[SerializeField] private Substance createdSubstance;
+
+		public Substance CreatedSubstance
+		{
+			get { return this.createdSubstance; }
+		}
+	}
+}

370d960a8e7d93bdaa0f5a3e11a8ccfef3ac3075
Adam <adam@adamslair.net>
2016-07-23 16:29:27 +0200

    Potion Script

---
+++ UnforeseenConsequences/Assets/Scripts/Potion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+using UnforeseenConsequences.Chemistry;
+
+namespace UnforeseenConsequences
+{
+	public class Potion : MonoBehaviour
+	{
+		[SerializeField] private Image middleImage;
+		[SerializeField] private Substance content;
+
+		public Substance Content
+		{
+			get { return this.content; }
+			set { this.content = value; this.UpdateAppearance(); }
+		}
+
+		private void OnValidate()
+		{
+			this.UpdateAppearance();
+		}
+
+		private void UpdateAppearance()
+		{
+			if (this.middleImage == null) return;
+			if (this.content == null)
+			{
+				this.middleImage.color = new Color(0, 0, 0, 0);
+			}
+			else
+			{ 
+				this.middleImage.color = this.content.Color;
+			}
+		}
+	}
+}
